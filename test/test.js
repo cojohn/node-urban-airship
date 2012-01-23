5e52aad46013556e824cb76156dcc97952243653
Dave Winders <david.winders@gmail.com>
2012-01-23 16:59:33 +0000

    Change to allow all Push Notif. API's be called.

--- test/test.js
+++ test/test.js
@@ -1,10 +1,35 @@
 var UA = require("../lib/urban-airship"),
 	ua = new UA("<api key>", "<api secret key>", "<api master key>");
 
+var payload0 = {
+	"device_tokens": ['device_id(s)'],
+	"aps": {
+		"alert": alert,
+		"badge": badge
+	}
+};
+
 ua.registerDevice("<token>", function(error) {
-	ua.pushNotification("<token", "Hi!", 42, function(error) {
+	ua.pushNotification("/api/push", payload0, function(error) {
 		ua.unregisterDevice("<token>", function(error) {
-			process.exit();
+
 		});
 	});
+});
+
+var payload1 = {
+	"aps": {
+		 "badge": 15,
+		 "alert": "Calling Urban Airship!",
+		 "sound": "cat.caf"
+	}/*,
+	"exclude_tokens": [
+		"device token you want to skip",
+		"another device token you want to skip"
+	]*/
+};
+
+
+ua.pushNotification("/api/push/broadcast/", payload1, function(error) {
+
 });
